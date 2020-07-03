23cbef54aa5c7c1b52112474aa199bdb2e8a8c87
Jevin Menezes <menezesjevin@gmail.com>
2020-07-03 20:55:08 +0530

    for one and two numbers

--- src/main/java/tdd/StringCalculator.java
+++ src/main/java/tdd/StringCalculator.java
@@ -1,10 +1,20 @@
 package tdd;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringCalculator 
 {
     public int add(String numbers) {
         if(numbers != null && numbers == "") {
             return 0;
+        }
+        if(StringUtils.isNotBlank(numbers)) {
+            String[] numbersList = numbers.split(",");
+            int result = 0;
+            for(int i = 0; i < numbersList.length; i++) {
+                result = result + Integer.parseInt(numbersList[i]);
+            }
+            return result;
         }
         return -1;
     }
